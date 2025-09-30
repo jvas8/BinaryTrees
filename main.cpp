ebb365366aac7d18217c401ed8b41346bb0dcd02
Jeimy Vasquez <2024161429@ub.edu.bz>
2025-09-30 16:24:04 +0000

    feature test in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <queue>
+using namespace std;
 
 struct BinaryTreeNode {
     string data;
@@ -13,13 +15,13 @@ struct BinaryTreeNode {
     }
 };
 
-void inorder(BinaryTreeNode* root) {
+void inOrder(BinaryTreeNode* root) {
     if (root == nullptr){
         return;
     }
-    inorder(root->left);
+    inOrder(root->left);
     cout << root->data << " ";
-    inorder(root->right);
+    inOrder(root->right);
 }
 
 void preOrder(BinaryTreeNode* root) {
@@ -27,27 +29,19 @@ void preOrder(BinaryTreeNode* root) {
         return;
     }
     cout << root->data << " ";
-    inorder(root->left);
-    inorder(root->right);
-}
-void preOrder(BinaryTreeNode* root) {
-    if (root == nullptr){
-        return;
-    }
-    cout << root->data << " ";
-    inorder(root->left);
-    inorder(root->right);
+    inOrder(root->left);
+    inOrder(root->right);
 }
 void postOrder(BinaryTreeNode* root) {
     if (root == nullptr){
         return;
     }
-    inorder(root->left);
-    inorder(root->right);
+    inOrder(root->left);
+    inOrder(root->right);
     cout << root->data << " ";
 
 }
-void levelorder(BinaryTreeNode* root) {
+void levelOrder(BinaryTreeNode* root) {
     if (root == nullptr) {
         return;
     }
@@ -66,6 +60,35 @@ void levelorder(BinaryTreeNode* root) {
       // Enqueue right child if it exists
         if (current->right) { q.push(current->right);}
     }
+}
+
+int main(){
+    BinaryTreeNode* root = new BinaryTreeNode("A");
+    root->left = new BinaryTreeNode("B");
+    root->right = new BinaryTreeNode("C");
+    root->left->left = new BinaryTreeNode("D");
+    root->left->right = new BinaryTreeNode("E");
+    root->right->left = new BinaryTreeNode("F");
+
+    cout << "In-order:   ";
+    inOrder(root);
+    cout << "\n";
+
+    cout << "Pre-order:  ";
+    preOrder(root);
+    cout << "\n";
+
+    cout << "Post-order: ";
+    postOrder(root);
+    cout << "\n";
+
+    cout << "Level-order: ";
+    levelOrder(root);
+    cout << "\n";
+
+
+
+    return 0;
 }
 
 
