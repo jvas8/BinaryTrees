6c835eefb916228d0eece12b308848d0c073f6cd
Jeimy Vasquez <2024161429@ub.edu.bz>
2025-09-30 16:11:19 +0000

    Implemented in order

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+struct BinaryTreeNode {
+    string data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+
+    BinaryTreeNode(const string& value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void inorder(BinaryTreeNode* root) {
+    if (root == nullptr){
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+
